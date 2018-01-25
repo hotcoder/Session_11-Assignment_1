02cab75f6af3fd33fbf4e0cc838d25aafa6d8949
hotcoder <hotcoder@users.noreply.github.com>
2018-01-25 10:03:49 +0530

    Update previous_zero_couny.py

--- previous_zero_couny.py
+++ previous_zero_couny.py
@@ -1,7 +1,17 @@
-df = pd.DataFrame({'X': [7, 2, 0, 3, 4, 2, 5, 0, 3, 4]})
+import pandas as pd
 
-# make a new column with zeros at zeros and nans elsewhere
-df = df.assign(idx_from_0=df.loc[df.X==0])
+l = [7, 2, 0, 3, 4, 2, 5, 0, 3, 4]
+i = 0
+r = []
 
-nul = df['idx_from_0'].isnull()
-df.assign(idx_from_0=nul.groupby((nul.diff() == 1).cumsum()).cumsum())
+for element in l:
+    if element != 0:
+        i += 1
+    else:
+        i = 0
+    r.append(i)
+    
+    
+result_data_frame = pd.DataFrame({"source":l,"result":r})
+
+print(result_data_frame)
